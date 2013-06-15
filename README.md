ac3fa0ff9f084379a09ee66d73178935d76e40dd
Deniz Eser <srwiez@gmail.com>
2013-06-15 20:21:01 +0200

    First real commit

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-minecraft-realtime
-==================
+# minecraft-realtime #
 
-Minecraft time syncronized to real time
+## Description ##
+Minecraft time syncronized to real time.
+
+## Requirement ##
+Minecraft server (>= 1.6) with rcon enabled and configured
+
+## How to use ##
+- Edit **realtime.ini**
+- Launch **php realtime.php**
+
+## TODO ##
+- Launch in deamon (unix only)
+- Parse configuration to find errors
+- Logs errors
+- More options
+	- set time if user is in the game
+	- configuration with in command line
+- etc..
+
+## Remerciements ##
+**ivkos** for **minecraftRcon.class.php** ( [https://github.com/ivkos/Minecraft-Query-for-PHP](https://github.com/ivkos/Minecraft-Query-for-PHP "Minecraft-Query-for-PHP") )
+
+## License ##
+This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+To view a copy of this license, visit [http://creativecommons.org/licenses/by-nc-sa/3.0/](http://creativecommons.org/licenses/by-nc-sa/3.0/)
---
+++ realtime.php
@@ -0,0 +1,124 @@
+<?php
+
+require __DIR__ . '/libs/minecraftRcon.class.php';
+
+class RealTimeMinecraft
+{
+	private $time;
+	private $lastTime;
+	private $minecraftTime;
+	private $lastMinecraftTime;
+	
+	private $rcon; // The RCon instance
+	
+	public $config; // Configuration of the deamon
+	
+	private $run = TRUE; // You want to stop the infinit loop ?
+	
+	const REAL_DAY = 86400; // Number of seconds in one real day
+	const MINECRAFT_DAY = 24000; // Number of ticks in one minecraft day
+	const DELAY = 21600; // Number of seconds of delay (Because 0 ticks isn't 00h00 in a real day)
+	
+	// Construct function
+	public function __construct($cmdConfig = array())
+	{
+		$fileConfig = parse_ini_file('realtime.ini');
+		
+		// Initialisation
+		$this->configInit($cmdConfig, $fileConfig);
+
+		// Rcon connection
+		$this->connect();
+	}
+	
+	// Initialisation function
+	private function configInit($cmdConfig, $fileConfig)
+	{
+		// Variables init
+		$this->time = 0;
+		$this->lastTime = 0;
+		$this->minecraftTime = 0;
+		$this->lastMinecraftTime = 0;
+		
+		$this->config = array_merge($fileConfig, $cmdConfig);
+	
+		return TRUE;
+	}
+	
+	// RCon connection function
+	private function connect()
+	{
+		$this->rcon = new minecraftRcon();
+		
+		try
+		{
+			$this->rcon->connect($this->config['hostname'], $this->config['port'], $this->config['password'], intval($this->config['timeout']));
+			$result = $this->rcon->command('gamerule doDaylightCycle false');
+			
+			if($result == "No game rule called 'doDaylightCycle' is available")
+			{
+				// TODO : make this better with an Exception or Logs
+				echo "Minecraft server version must be >= 1.6";
+				die();
+			}
+	
+			return TRUE;
+		}
+		catch( Exception $e )
+		{
+			echo $e->getMessage( );
+	
+			return FALSE;
+		}
+	}
+	
+	// unix signals
+	public function signals($signal)
+	{
+		// TODO : mke this function
+		// kill -> $this->run = false;
+		// reload -> $this->init();
+	}
+	
+	// Gooooo !
+	public function step()
+	{
+		// Real time day between 0 and 86400
+		$daytime = mktime(0, 0, 0, date('n'), date('j'), date('Y'));
+		$this->time = time()-$daytime;
+		
+		// Minecraft time calculations
+		$this->minecraftTime = intval(($this->time-self::DELAY)*self::MINECRAFT_DAY/self::REAL_DAY);
+		if($this->minecraftTime < 0) $this->minecraftTime = MINECRAFT_DAY+$this->minecraftTime;
+		
+		// If y need to change the time on minecraft
+		if($this->time != $this->lastTime && $this->minecraftTime != $this->lastMinecraftTime)
+		{
+			// Send RCon with $minecraftTime
+			$this->rcon->command('time set '.$this->minecraftTime);
+			//echo "Send : ".$this->minecraftTime."\n";
+			
+			// Update "last" variables
+			$this->lastTime = $this->time;
+			$this->lastMinecraftTime = $this->minecraftTime;
+		}
+	}
+	
+	// Gooooo !
+	public function run()
+	{
+		// Infinity loop
+		while($this->run)
+		{
+			// make a step forward
+			$this->step();
+			
+			// Wait a second
+			sleep(1);
+		}
+	}
+}
+
+$instance = new RealTimeMinecraft();
+$instance->run();
+?>
